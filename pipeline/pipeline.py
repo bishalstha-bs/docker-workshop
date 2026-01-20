e0442fba19898d12e5284c4141998e40fc1013e7
unknown <Administrator@hei-test-pc.hei.com>
2026-01-20 15:54:24 +0545

    New code inside the pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,3 +1,12 @@
 import sys
 
-print('hello pipeline')
+import pandas as pd
+
+print('arguments', sys.argv)
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month'] = month
+
+print(df.head())
+print(f'hello pipeline, month={month}')
